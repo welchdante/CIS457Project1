5b1f944398c83eecc6ea6b985a48717cdfb9ba77
Dante Welch <welchdan@mail.gvsu.edu>
2018-10-01 09:36:33 -0400

    handle packet duplication and reordering client side

--- Client/client.py
+++ Client/client.py
@@ -18,47 +18,69 @@ class Client():
         print('\n------------------------------------------------\n')
 
         self.send_filename(filename)
+
+        print('Receiving data...')
+
+        #makes the client eventually close when there is no data
+        self.sock.setblocking(0)
+        begin = time.time()
+        timeout = 2
+        expected = 0
+        packets = []
+
+        while True:
+            # wait if you have no data
+            if time.time() - begin > timeout:
+                break
+            #recieve something
+            try:
+                packet = self.sock.recv(1024)
+                if packet: 
+                    num, data = self.extract(packet)
+                    print("Got packet ", num)
+
+                    # Send acknlowedgement to the sender
+                    if num == expected:
+                        print("Sending acknlowedgement ", expected)
+                        self.send_filename(str(expected))
+                        expected += 1
+                        packets.append((num, data))
+                            
+                    else:
+                        print("Sending acknlowedgement ", (expected - 1))
+                        self.send_filename(str(expected - 1))
+                        
+                    begin = time.time()
+                    
+                else: 
+                    time.sleep(0.01)
+
+            except socket.error as err:
+                pass
+
+        # sort packets, handle reordering
+        sorted(packets, key=lambda x: x[0])
+
+        packets = self.handle_duplicates(packets)
+
+        write file
         with open(filename, 'wb') as f:
             print('File opened')
-            print('Receiving data...')
-
-            #makes the client eventually close when there is no data
-            self.sock.setblocking(0)
-            begin = time.time()
-            timeout = 2
-            expected = 0
-
-            while True:
-                # wait if you have no data
-                if time.time() - begin > timeout:
-                    break
-                #recieve something
-                try:
-                    packet = self.sock.recv(1024)
-                    if packet: 
-                        num, data = self.extract(packet)
-                        print("Got packet ", num)
-
-                        # Send acknlowedgement to the sender
-                        if num == expected:
-                            print("Sending acknlowedgement ", expected)
-                            self.send_filename(str(expected))
-                            expected += 1
-                            f.write(data)
-                        else:
-                            print("Sending acknlowedgement ", (expected - 1))
-                            self.send_filename(str(expected - 1))
-                        
-                        begin = time.time()
-                    
-                    else: 
-                        time.sleep(0.01)
-
-                except socket.error as err:
-                    pass
+            for p in packets:
+                data = p[1]
+                f.write(data)
 
         f.close()
         self.end_connection()
+
+    def handle_duplicates(self, packets):
+        i = 0
+        while i < len(packets) - 1:
+            if packets[i][0] == packets[i+1][0]:
+                del packets[i+1]
+            else:
+                i += 1
+        return packets
 
     def send_filename(self, filename):
         return self.sock.sendto(filename.encode(), (self.host, self.port))
